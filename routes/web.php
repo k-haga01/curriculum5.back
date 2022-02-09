e740d681cfbc81e300734366e5b7866e5c676287
k-haga01 <keitahaga.118@gmail.com>
2022-02-09 18:24:26 +0900

    bladeテンプレートの継承

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'App\Http\Controllers\Admin\NewsController@add');
 });
